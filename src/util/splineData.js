ab41a2be8acf028eb709ebf4972a8f3931d17908
Abdurehman Saeed <asaeed526@gmail.com>
2022-10-16 19:09:39 +0300

    spliine practice edited

--- src/util/splineData.js
+++ src/util/splineData.js
@@ -1,21 +1,24 @@
+// import Spline from '@splinetool/react-spline'
+
+const basicUrl="https://my.spline.design/"
 
 export const listOfSplineData=[
 {
  "title":'home',
- "url":'https://my.spline.design/myworkspace-52c2bc639deab6d962b7704d01108ad4/'
+ "url":basicUrl+'myworkspace-52c2bc639deab6d962b7704d01108ad4/'
 },
 {
  "title":'switch',
- "url":'https://my.spline.design/switch-d39dc7fbf6e98bbe2523340ea9f39f14/'
+ "url":basicUrl+'switch-d39dc7fbf6e98bbe2523340ea9f39f14/'
 },
 {
  "title":'keyBoard',
- "url":'https://my.spline.design/keyboard-4082b63b2dd6f9fbd19fb5d4bcfb7533/'
+ "url":basicUrl+'keyboard-4082b63b2dd6f9fbd19fb5d4bcfb7533/'
 },
 
 {
  "title":'myHome',
- "url":'https://my.spline.design/toolscoolection-92b331e51fb7300618b48dfc8b497680/'
+ "url":basicUrl+'toolscoolection-92b331e51fb7300618b48dfc8b497680/'
 },
 
 ]
